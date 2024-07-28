b3e4ddc68e15172124c9cb3e66d64551ba8647bd
river <laolei@forkway.cn>
2024-07-28 19:27:20 +0800

    feat: add debug logging to BgAliveManager class

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,50 @@
+/* eslint-disable no-console */
 export class BgAliveManager {
   private interval: number
   private intervalId: number | null
   private runtime: typeof chrome.runtime
+  private isDebug: boolean
 
-  constructor(interval: number = 23000) {
+  constructor(interval: number = 23000, isDebug: boolean = false) {
     const browserGlobal = globalThis as any
     this.runtime = (browserGlobal.browser?.runtime || browserGlobal.chrome?.runtime) as typeof chrome.runtime
     this.interval = interval
     this.intervalId = null
+    this.isDebug = isDebug
   }
-
-  public start(): void
-  public start(interval: number): void
 
   public start(interval?: number): void {
     if (interval !== undefined)
       this.interval = interval
 
-    if (!this.intervalId) {
+    if (this.intervalId === null) {
       this.executePlatformInfo()
       this.intervalId = globalThis.setInterval(() => this.executePlatformInfo(), this.interval) as unknown as number
+      this.debugLog('BgAliveManager started')
     }
   }
 
-  // 停止活跃
   public stop(): void {
     if (this.intervalId !== null) {
       clearInterval(this.intervalId)
       this.intervalId = null
+      this.debugLog('BgAliveManager stopped')
     }
   }
 
-  // 封装平台信息获取方法，增加错误处理
+  private debugLog(...msg: any[]): void {
+    if (this.isDebug)
+      console.log(...msg)
+  }
+
   private executePlatformInfo(): void {
     try {
       this.runtime.getPlatformInfo((info: chrome.runtime.PlatformInfo) => {
-        // console.log('Platform info:', info);
-        return info
+        this.debugLog('Platform info:', info)
       })
     }
     catch (error) {
-      console.error('Error fetching platform info:', error as Error)
+      console.error('Error fetching platform info:', error)
       this.stop()
     }
   }
